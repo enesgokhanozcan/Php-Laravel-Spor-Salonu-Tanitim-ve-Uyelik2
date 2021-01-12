0d6cd3a6b0c0d6ead3484e0764faa9864e4eb2ea
Antropozen <enes.gokhan.ozcan@gmail.com>
2021-01-12 06:05:50 +0300

    Admin Panel Template Uyarlaması

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,4 +10,8 @@ class HomeController extends Controller
     {
         return view('home.index');
     }
+    public function aboutus()
+    {
+        return view('home.about');
+    }
 }
---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.admin')
+@section('title','Admin Panel Homepage')
+@section('content')
+    @include('admin._content')
+@endsection
+
